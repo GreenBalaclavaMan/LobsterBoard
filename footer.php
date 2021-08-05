436da4e2819248aa46f4c2eb7c6d5ee2aa8d8420
GreenBalaclavaMan <GreenBalaclavaMan@protonmail.com>
2021-08-05 15:07:06 -0400

    Updated Github Link

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 		<!--Footer-->
 		<div class="div-footer">
 			<p>
-				This project is open source, <a href="https://github.com/GreenBalaclavaMan/WebOTP">check out WebOTP on github</a>.
+				This project is open source, <a href="https://github.com/GreenBalaclavaMan/LobsterBoard">check out LobsterBoard on github</a>.
 			</p>
 			<p>
 				&copy;2021 Nicholas Rinella
